0830321c26529084c160faef15371b473a9e9739
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-05-30 09:27:38 +0600

    update print statement

--- synchronous/consumers.py
+++ synchronous/consumers.py
@@ -4,24 +4,24 @@ from channels.consumer import SyncConsumer, AsyncConsumer
 class MySyncConsumer(SyncConsumer):
     """SyncConsumer"""
 
-    def websokcet_connect(self, event):
-        print("WebSocket Connect...")
+    def websocket_connect(self, event):
+        print("WebSocket Connect...", event)
 
     def websocket_receive(self, event):
-        print("WebSocket Received...")
+        print("WebSocket Received...", event)
 
     def websocket_disconnect(self, event):
-        print("WebSocket Disconnect...")
+        print("WebSocket Disconnect...", event)
 
 
 class MyAsyncConsumer(AsyncConsumer):
     """SyncConsumer"""
 
-    async def websokcet_connect(self, event):
-        print("WebSocket Connect...")
+    async def websocket_connect(self, event):
+        print("WebSocket Connect...", event)
 
     async def websocket_receive(self, event):
-        print("WebSocket Received...")
+        print("WebSocket Received...", event)
 
     async def websocket_disconnect(self, event):
-        print("WebSocket Disconnect...")
+        print("WebSocket Disconnect...", event)
