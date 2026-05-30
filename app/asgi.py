54d9b293c0487549a2bd7f1ff7f067eba5a3d5ab
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-05-30 09:29:04 +0600

    integrate websocket url with asgi

--- app/asgi.py
+++ app/asgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import synchronous.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': URLRouter(
+        synchronous.routing.websocket_urlpatterns 
+    )
 })
