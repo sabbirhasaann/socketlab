1509de74ca7278ad817fbb833c4080676a5fee13
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-10 17:52:13 +0600

    feat: real time data transmission

--- asynchronous/consumers.py
+++ asynchronous/consumers.py
@@ -1,5 +1,6 @@
 from channels.consumer import AsyncConsumer
 from channels.exceptions import StopConsumer
+from asyncio import sleep
 
 
 class MyAsyncConsumer(AsyncConsumer):
@@ -14,10 +15,12 @@ class MyAsyncConsumer(AsyncConsumer):
     async def websocket_receive(self, event):
         print("WebSocket Received...", event)
         print("Message is ", event['text'])
-        await self.send({
-            'type': 'websocket.send',
-            'text': 'Message from async server'
-        })
+        for i in range(50):
+            await self.send({
+                'type': 'websocket.send',
+                'text': str(i)
+            })
+            await sleep(1)
 
     async def websocket_disconnect(self, event):
         print("WebSocket Disconnect...", event)
