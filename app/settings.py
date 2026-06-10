198267cda5bc5bd2ae365b7bacdbe439060566b0
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-10 22:05:18 +0600

    added: redis channel layers

--- app/settings.py
+++ app/settings.py
@@ -117,3 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": os.environ.get('REDIS_CHANNEL'),
+        "CONFIG": {
+            "hosts": [(os.environ.get('REDIS_HOST'), os.environ.get('REDIS_PORT'))],
+        }
+    }
+}
