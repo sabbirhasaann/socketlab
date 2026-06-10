36ffc1047651ad733c49e345eb5afee234e3bb54
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-10 21:51:14 +0600

    feat: sync consumer for chat app

---
+++ chat/consumers.py
@@ -0,0 +1,23 @@
+from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.exceptions import StopConsumer
+
+
+class MySyncConsumer(SyncConsumer):
+    """my sync consumer"""
+
+    def websocket_connect(self, event):
+        print('Websocket is connected...')
+        self.send({
+            "type": 'websocket.accept'
+        })
+
+    def websocket_receive(self, event):
+        print("Message received", event['text'])
+        self.send({
+            'type': 'websocket.send',
+            'text': 'Message sent from sync 1 server'
+        })
+
+    def websocket_disconnect(self, event):
+        print('Websocket disconnected...')
+        StopConsumer()
