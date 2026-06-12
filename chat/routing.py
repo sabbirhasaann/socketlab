acb9db75dddcf44bc62804eb6dd6185d2ff78d27
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-12 09:32:16 +0600

    url: async consumer router

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .consumers import MySyncConsumer
+from .consumers import MySyncConsumer, MyAsyncConsumer
 
 websocket_urlpatterns = [
-    path('ws/sc1/', MySyncConsumer.as_asgi(), name='sync-consumer-1')
+    path('ws/sc1/', MySyncConsumer.as_asgi(), name='sync-consumer-1'),
+    path('ws/ac1/', MyAsyncConsumer.as_asgi(), name='async-consumer-1')
 ]
