c98091b508cec44e3cd6a71faa53c9ce2bf1597c
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-15 20:24:10 +0600

    feat: dynamic group in routing

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,6 @@ from .consumers import MySyncConsumer, MyAsyncConsumer
 
 websocket_urlpatterns = [
     path('ws/sc1/', MySyncConsumer.as_asgi(), name='sync-consumer-1'),
-    path('ws/ac1/', MyAsyncConsumer.as_asgi(), name='async-consumer-1')
+    path('ws/ac1/<str:group_name>/',
+         MyAsyncConsumer.as_asgi(), name='async-consumer-1')
 ]
