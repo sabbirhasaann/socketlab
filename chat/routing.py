54c70d560b4d96e76393e5a09548e7e31f78b4fb
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-15 21:02:33 +0600

    add dynamic group name in route

--- chat/routing.py
+++ chat/routing.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .consumers import MySyncConsumer, MyAsyncConsumer
 
 websocket_urlpatterns = [
-    path('ws/sc1/', MySyncConsumer.as_asgi(), name='sync-consumer-1'),
+    path('ws/sc1/<str:group_name>/',
+         MySyncConsumer.as_asgi(), name='sync-consumer-1'),
     path('ws/ac1/<str:group_name>/',
          MyAsyncConsumer.as_asgi(), name='async-consumer-1')
 ]
