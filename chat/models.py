c710200f47ddf635d06713772d7e6fcec725db13
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-17 19:24:01 +0600

    feat: added group and chat model in database

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Chat(models.Model):
+    content = models.CharField(max_length=1000)
+    timestamps = models.DateTimeField(auto_now=True)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+
+    def __str__(self):
+        self.content
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
