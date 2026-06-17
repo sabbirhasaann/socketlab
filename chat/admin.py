cbe18b2892e5a21a298eebbe05d2267519868fdf
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-17 19:25:15 +0600

    feat: added chat and group models to admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Chat, Group
 
-# Register your models here.
+
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content', 'timestamps', 'group']
+    ordering = ['-timestamps']
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+admin.site.register(Chat, ChatAdmin)
+admin.site.register(Group, GroupAdmin)
