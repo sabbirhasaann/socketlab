df458321c727dfb29ea59ecc64c9a1491b841df1
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-06-17 20:19:23 +0600

    fix: default settings to production

--- app/asgi.py
+++ app/asgi.py
@@ -11,8 +11,10 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from app.routing import websocket_urlpatterns
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+load_dotenv()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings.production')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
