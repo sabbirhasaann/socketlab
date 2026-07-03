42665b3b808acb88e05aa3ddb41950163f6222e9
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-07-03 18:29:40 +0600

    feat: real time data transmission

--- genconsumer/consumers.py
+++ genconsumer/consumers.py
@@ -1,4 +1,6 @@
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+from time import sleep
+from asyncio import sleep as asysleep
 
 
 class MyWebsocketConsumer(WebsocketConsumer):
@@ -10,7 +12,11 @@ class MyWebsocketConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
         print("Message received from client...", text_data)
-        self.send(text_data="Message from server to client")
+        # self.send(text_data="Message from server to client")
+
+        for i in range(10):
+            self.send(text_data=str(i))
+            sleep(1)
 
     def disconnect(self, close_code):
         print("Websocket disconnected...", close_code)
@@ -25,7 +31,10 @@ class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data=None, bytes_data=None):
         print("Message received from client...", text_data)
-        await self.send(text_data="Message from server to client")
+        # await self.send(text_data="Message from server to client")
+        for i in range(10):
+            await self.send(text_data=str(i))
+            await asysleep(1)
 
     async def disconnect(self, close_code):
         print("Websocket disconnected...", close_code)
